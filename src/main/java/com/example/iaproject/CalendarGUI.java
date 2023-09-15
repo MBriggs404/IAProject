5958c4e22c747070ff1cb060f335e2046cd2a9e4
17Briggs_M <17Briggs_M@bexleygs.co.uk>
2023-09-15 11:45:53 +0100

    MAKE CALENDAR FIND ENTRIES

--- src/main/java/com/example/iaproject/CalendarGUI.java
+++ src/main/java/com/example/iaproject/CalendarGUI.java
@@ -1,12 +1,30 @@
 package com.example.iaproject;
 
+import com.calendarfx.model.CalendarEvent;
 import javafx.fxml.FXML;
 import com.calendarfx.model.Calendar;
 import com.calendarfx.model.CalendarSource;
-import com.calendarfx.model.Entry;
 import com.calendarfx.view.CalendarView;
 
 public class CalendarGUI {
     @FXML
     private CalendarView calendarView;
+
+    @FXML
+
+    private void initialize() {
+        CalendarSource cs = new CalendarSource("cs");
+        calendarView.getCalendarSources().add(cs);
+//TODO: Make calendar, add to valview and findEntries("")
+        com.calendarfx.model.Calendar cal = new Calendar("cal");
+        cal = cs.getCalendars().get(0);
+        cal.findEntries("");
+
+        calendarView.addEventHandler(CalendarEvent.ENTRY_CHANGED, e -> handleEvent(e));
+
+    }
+
+    private void handleEvent(Object e) {
+        System.out.printf("Entry changed");
+    }
 }
